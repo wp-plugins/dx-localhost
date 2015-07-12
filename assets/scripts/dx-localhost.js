a8bcd5d694ba0166ea58d0390e5ae0c21e6a9e88
nofearinc <nofearinc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-12 12:12:46 +0000

    DX-localhost version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/dx-localhost/trunk@1197468 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/scripts/dx-localhost.js
+++ assets/scripts/dx-localhost.js
@@ -1,11 +1,8 @@
-/**
- * @author nofearinc
- * 
-*/
 jQuery( document ).ready( function( $ ) {
 	var notice = dxlocalhost.notice_msg;
+	
 	$('body').append( '<div id="dx-localhost-notice">' + notice + '</div>' );
-	if ( $('#wpadminbar').length ) {
+	if ( $('#wpadminbar').length && parseInt( noticeline.disable_notice, 10) == 0 ) {
 		$('#dx-localhost-notice').css('top', '32px');
 	}
 });
